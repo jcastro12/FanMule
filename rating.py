a0d8ec7c12f350bc7eb0d9b8762ec0cc30497021
Roy Hsu <hjuiteng@gmail.com>
2024-04-17 20:01:59 -0400

    Updates each script to be callable through CLI with arguements

--- rating.py
+++ rating.py
@@ -1,58 +1,92 @@
+"""
+    Updates rating for a given sport using the accurating library
+    Usage: python rating.py --sport <sport_code>
+"""
+
 import accurating
+import argparse
+import json
+import os
+
+# Parse command line arguments
+parser = argparse.ArgumentParser(
+    description="""
+    Updates rating for a given sport in accordance to the NCAA defiend sport codes.
+    See https://ncaaorg.s3.amazonaws.com/championships/resources/common/NCAA_SportCodes.pdf for the list of sport codes
+    """
+)
+parser.add_argument("--sport", type=str, help="Sport code to scrape")
+args = parser.parse_args()
 
 # Sport to scrape
 # See https://ncaaorg.s3.amazonaws.com/championships/resources/common/NCAA_SportCodes.pdf for the list of sport codes
-sport_code = 'MBA'
+sport_code = args.sport
+
 matches = []
-with open(f'{sport_code}_2023.txt', 'r') as f:
-    for line in f:
-        line = line.strip().split(':')
-        matches.append(((line[0], int(line[1])), (line[2], int(line[3]))))
+
+file_path = f"{sport_code}_2023.txt"
+if os.path.exists(file_path):
+    with open(file_path, "r") as f:
+        for line in f:
+            line = line.strip().split(":")
+            matches.append(((line[0], int(line[1])), (line[2], int(line[3]))))
+else:
+    print(
+        f"File '{file_path}' does not exist. Run scrape.py first to generate the file. Or check if the sport code is correct."
+    )
+
 
 match_dict = []
 for match in matches:
     event = dict()
-    event['season'] = 0
-    event['p1'] = match[0][0]
-    event['p2'] = match[1][0]
+    event["season"] = 0
+    event["p1"] = match[0][0]
+    event["p2"] = match[1][0]
     s1 = match[0][1]
     s2 = match[1][1]
     if s1 > s2:
-        event['winner'] = match[0][0]
+        event["winner"] = match[0][0]
     else:
-        event['winner'] = match[1][0]
+        event["winner"] = match[1][0]
     match_dict.append(event)
 
 matches = []
-with open(f'{sport_code}_2024.txt', 'r') as f:
-    for line in f:
-        line = line.strip().split(':')
-        matches.append(((line[0], int(line[1])), (line[2], int(line[3]))))
+
+file_path = f"{sport_code}_2024.txt"
+if os.path.exists(file_path):
+    with open(file_path, "r") as f:
+        for line in f:
+            line = line.strip().split(":")
+            matches.append(((line[0], int(line[1])), (line[2], int(line[3]))))
+else:
+    print(
+        f"File '{file_path}' does not exist. Run scrape.py first to generate the file. Or check if the sport code is correct."
+    )
+
 
 for match in matches:
     event = dict()
-    event['season'] = 1
-    event['p1'] = match[0][0]
-    event['p2'] = match[1][0]
+    event["season"] = 1
+    event["p1"] = match[0][0]
+    event["p2"] = match[1][0]
     s1 = match[0][1]
     s2 = match[1][1]
     if s1 > s2:
-        event['winner'] = match[0][0]
+        event["winner"] = match[0][0]
     else:
-        event['winner'] = match[1][0]
+        event["winner"] = match[1][0]
     match_dict.append(event)
 
 resultArray = accurating.data_from_dicts(match_dict)
 config = accurating.Config(
-        season_rating_stability=100,
-        smoothing=0.1,
-        # max_steps=100000,
-        # do_log=True,
-    )
+    season_rating_stability=100,
+    smoothing=0.1,
+    # max_steps=100000,
+    # do_log=True,
+)
 model = accurating.fit(resultArray, config)
 
 # Dumps the rating dictionary to a json file
-import json
 collapsed_dict = {}
 for key, inner_dict in model.rating.items():
     # collapsed_dict[key] = inner_dict[1]
@@ -61,5 +95,5 @@ for key, inner_dict in model.rating.items():
 
 sorted_dict = dict(sorted(collapsed_dict.items(), key=lambda x: x[1], reverse=True))
 
-with open(f'accurating_{sport_code}.json', 'w') as f:
+with open(f"accurating_{sport_code}.json", "w") as f:
     json.dump(sorted_dict, f, indent=4)
--- scrapper.py
+++ scrapper.py
@@ -1,58 +1,93 @@
+"""
+    Updates match result for a given sport using beautifulsoup. Assumes end date is today or 2024-06-15. 
+    Usage: python scrapper.py --sport <sport_code>
+"""
+
 import datetime as dt
 import requests
 from bs4 import BeautifulSoup
+import argparse
 
-# Sport to scrape
-# See https://ncaaorg.s3.amazonaws.com/championships/resources/common/NCAA_SportCodes.pdf for the list of sport codes
-sport_code = 'WLA' 
+# Parse command line arguments
+parser = argparse.ArgumentParser(
+    description="""
+    Updates match result for a given sport in 2024 using beautifulsoup. Assumes end date is today or 2024-06-15. 
+    See https://ncaaorg.s3.amazonaws.com/championships/resources/common/NCAA_SportCodes.pdf for the list of sport codes
+    """
+)
+parser.add_argument("--sport", type=str, help="Sport code to scrape")
+args = parser.parse_args()
+
+sport_code = args.sport
 
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) ApplseWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) ApplseWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+}
 
 # See https://stats.ncaa.org/contests/livestream_scoreboards for appropriate date range
 
 # Start date
-date = dt.datetime(2024, 2, 2)
+date = dt.datetime(2024, 2, 1)
 
-while date < dt.datetime(2024, 4, 11): # End date
+# End date
+end_date = min(dt.datetime.today(), dt.datetime(2024, 6, 15))
+
+# List to store the results
+results = []
+
+while date < end_date:  
     date += dt.timedelta(days=1)
-    print(date.month, date.day, sep='/')
-    
-    link = 'https://stats.ncaa.org/contests/livestream_scoreboards?utf8=%E2%9C%93&sport_code={}&academic_year={}&division=3&game_date={:0>2}%2F{:0>2}%2F{}&commit=Submit'.format(sport_code, date.year, date.month, date.day, date.year)
-    
+    print(date.month, date.day, sep="/")
+
+    link = "https://stats.ncaa.org/contests/livestream_scoreboards?utf8=%E2%9C%93&sport_code={}&academic_year={}&division=3&game_date={:0>2}%2F{:0>2}%2F{}&commit=Submit".format(
+        sport_code, date.year, date.month, date.day, date.year
+    )
+
     response = requests.get(link, headers=headers)
 
     if response.status_code == 200:
-            # Parse the HTML content
-            soup = BeautifulSoup(response.text, 'html.parser')
-            soup = soup.find('table', {'class': 'livestream_table'})
-            
-            if soup == None: continue
-        
-            # Extract the table data
-            list = soup.find_all('tr')[1:]
-            with open(f'{sport_code}_{date.year}.txt', 'a') as f:
-                for index, row in enumerate(list):
-                    if index % 5 == 0: 
-                        teamA = row.find('a')
-                        teamB = list[index+3].find('a')
-                        if teamA == None or teamB == None:
-                            continue
+        # Parse the HTML content
+        soup = BeautifulSoup(response.text, "html.parser")
+        soup = soup.find("table", {"class": "livestream_table"})
 
-                        teamA = teamA.get_text(strip=True)
-                        if(teamA[0] == '#'): teamA = ' '.join(teamA.split(' ')[1:-1])
-                        else: teamA = ' '.join(teamA.split(' ')[:-1])
-                        
-                        teamB = teamB.get_text(strip=True)
-                        if teamB[0] == '#': teamB = ' '.join(teamB.split(' ')[1:-1])
-                        else: teamB = ' '.join(teamB.split(' ')[:-1])
+        if soup == None:
+            continue
 
-                        aScore = row.find('td', {'class': 'totalcol'})
-                        bScore = list[index + 3].find('td', {'class': 'totalcol'})
-                        if aScore == None or bScore == None:
-                            continue
-                        aScore = int(aScore.get_text(strip=True))
-                        bScore = int(bScore.get_text(strip=True))
-                        f.write(f"{teamA}:{aScore}:{teamB}:{bScore}\n")
+        # Extract the table data
+        list = soup.find_all("tr")[1:]
+        for index, row in enumerate(list):
+            if index % 5 == 0:
+                teamA = row.find("a")
+                teamB = list[index + 3].find("a")
+                if teamA == None or teamB == None:
+                    continue
+
+                teamA = teamA.get_text(strip=True)
+                if teamA[0] == "#":
+                    teamA = " ".join(teamA.split(" ")[1:-1])
+                else:
+                    teamA = " ".join(teamA.split(" ")[:-1])
+
+                teamB = teamB.get_text(strip=True)
+                if teamB[0] == "#":
+                    teamB = " ".join(teamB.split(" ")[1:-1])
+                else:
+                    teamB = " ".join(teamB.split(" ")[:-1])
+
+                aScore = row.find("td", {"class": "totalcol"})
+                bScore = list[index + 3].find("td", {"class": "totalcol"})
+                if aScore == None or bScore == None:
+                    continue
+                aScore = int(aScore.get_text(strip=True))
+                bScore = int(bScore.get_text(strip=True))
+                results.append((teamA, aScore, teamB, bScore))
     else:
         print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
+
+# Writes the results to a file
+with open(f"{sport_code}_{date.year}.txt", "w") as f:
+    f.write(
+        "\n".join(
+            [f"{result[0]}:{result[1]}:{result[2]}:{result[3]}" for result in results]
+        )
+    )
